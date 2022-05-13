117f9d6865dd3c5a7ecf76e9eab5d48f46e594b0
crf <wtl2ltw@163.com>
2022-05-13 08:54:17 +0800

    hot-fix

--- src/main/java/com/ltw/git/git.java
+++ src/main/java/com/ltw/git/git.java
@@ -4,5 +4,6 @@ public class git {
     public static void main(String[] args) {
         System.out.println("git");
         System.out.println("second git");
+        System.out.println("hot-fix");
     }
 }
